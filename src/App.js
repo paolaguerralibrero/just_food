318260eb65232e9910527178822836adb58270fa
Paolaguerralibrero <mpaolamcra@hotmail.com>
2023-03-24 21:35:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Fragment, useState } from "react";
+import Foods from "./Foods";
+export const foodItemsContext = React.createContext();
 
-function App() {
+
+const App = () => {
+  const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
+  const [menuItems, setMenuItems] = useState([
+    {
+      id: 1,
+      name: "Chicken Burger",
+      quantity: 40,
+      desc: "Fried chicken burger - lettuce, tomato, cheese and mayonnaise,",
+      price: "24",
+      image: "cb.jpg",
+    },
+    {
+      id: 2,
+      name: "Veg Burger",
+      quantity: 30,
+      desc: "Plant-based burger — lettuce, tomato, vegan cheese and mayonnaise",
+      price: "22",
+      image: "vb.jpg",
+    },
+    {
+      id: 3,
+      name: "Chips",
+      quantity: 50,
+      desc: "Potato chips fried to perfection",
+      price: "7",
+      image: "chips.jpg",
+    },
+    {
+      id: 4,
+      name: "Ice Cream",
+      quantity: 30,
+      desc: "Ice cream - Vanilla ice cream double scoop",
+      price: "4",
+      image: "ic.jpg",
+    },
+    {
+      id: 4,
+      name: "Croquetas",
+      quantity: 300,
+      desc: "Croquetas - Little pieces of heaven",
+      price: "4",
+      image: "cro.jpg",
+    },
+  ]);
+
+  const updateMenuItemQuantity = (id, orderQuantity) => {
+    const updatedMenuItems = menuItems.map((item) => {
+      if (item.id === id)
+        return {
+          ...item,
+          quantity: item.quantity - orderQuantity,
+        };
+      return item;
+    });
+    setMenuItems(updatedMenuItems);
+  };
+
   return (
+    <foodItemsContext.Provider value={menuItems}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="toggleButton"
+        onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
+      >
+        {isChooseFoodPage ? "Availability Check" : "Order Food"}
+      </button>
+      <h3 className="title">Just Food Online Shop</h3>
+      {!isChooseFoodPage && (
+        <Fragment>
+          <h4 className="subTitle">Menu Availability</h4>
+          <ul className="ulApp">
+            {menuItems.map((item) => {
+              return (
+                <li key={item.id} className="liApp">
+                  {item.name} - {item.quantity}
+                </li>
+              );
+            })}
+          </ul>
+        </Fragment>
+      )}
+
+
+      {isChooseFoodPage && 
+      <Foods 
+      foodItems={menuItems}
+      />}
+
     </div>
+    </foodItemsContext.Provider>
   );
-}
+};
 
 export default App;
