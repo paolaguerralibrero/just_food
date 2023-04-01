d51e3341462ccce22dd403d22a7f5dc47d542016
Paolaguerralibrero <mpaolamcra@hotmail.com>
2023-04-01 23:33:30 +0100

    added styling to food.js MediaQuery practive for responsive practice with react-responsive

--- src/Foods.js
+++ src/Foods.js
@@ -1,40 +1,47 @@
-import React, {Fragment, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import React, { Fragment, useState } from "react";
 import "./Foods.css";
 import FoodOrder from "./FoodOrder";
 
 const Foods = (props) => {
-    const [selectedFood, setSelectedFood] = useState("");
+  const [selectedFood, setSelectedFood] = useState("");
+  const isLapOrDeskTop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  
+  const isMobile = useMediaQuery({ query: "(max-width: 480px)" });
+  const handleSelect = (event) => {
+    setSelectedFood(
+      props.foodItems.find((item) => {
+        return item.id === parseInt(event.currentTarget.dataset.id);
+      })
+    );
+  };
 
-    const handleSelect = (event) => {
-        setSelectedFood(
-            props.foodItems.find((item) => {
-                return item.id === parseInt(event.currentTarget.dataset.id);
-            })
-        );
-    };
-
-    return (
+  return (
+    <>
+      {isMobile && (
         <Fragment>
           {!selectedFood && (
             <Fragment>
-              <h4 className="foodTitle">Choose from our Menu</h4>
-              <ul className="ulFoods">
+              <h4 className="foodTitleMobile">Choose from our Menu</h4>
+              <ul className="ulFoodsMobile">
                 {props.foodItems.map((item) => {
                   return (
                     <li
                       key={item.id}
-                      className="liFoods"
+                      className="liFoodsMobile"
                       data-id={item.id}
                       onClick={handleSelect}
                     >
                       <img
-                        className="foodImg"
+                        className="foodImgMobile"
                         src={require(`./images/${item.image}`)}
                         alt={item.name}
                       />
-                      <div className="foodItem">
-                        <p className="foodDesc">{item.desc}</p>
-                        <p className="foodPrice">{item.price}$</p>
+                      <div className="foodItemMobile">
+                        <p className="foodDescMobile">{item.desc}</p>
+                        <p className="foodPriceMobile">{item.price}$</p>
                       </div>
                     </li>
                   );
@@ -42,15 +49,55 @@ const Foods = (props) => {
               </ul>
             </Fragment>
           )}
+
           {selectedFood && (
             <FoodOrder
               food={selectedFood}
               returnToMenu={() => setSelectedFood("")}
-              
             />
           )}
         </Fragment>
-      );
-    };
-    
-    export default Foods;
+      )}
+      {isLapOrDeskTop&&(
+       <Fragment>
+       {!selectedFood && (
+         <Fragment>
+           <h4 className="foodTitle">Choose from our Menu</h4>
+           <ul className="ulFoods">
+             {props.foodItems.map((item) => {
+               return (
+                 <li
+                   key={item.id}
+                   className="liFoods"
+                   data-id={item.id}
+                   onClick={handleSelect}
+                 >
+                   <img
+                     className="foodImg"
+                     src={require(`./images/${item.image}`)}
+                     alt={item.name}
+                   />
+                   <div className="foodItem">
+                     <p className="foodDesc">{item.desc}</p>
+                     <p className="foodPrice">{item.price}$</p>
+                   </div>
+                 </li>
+               );
+             })}
+           </ul>
+         </Fragment>
+       )}
+
+       {selectedFood && (
+         <FoodOrder
+           food={selectedFood}
+           returnToMenu={() => setSelectedFood("")}
+         />
+       )}
+     </Fragment>
+      )}
+    </>
+  );
+};
+
+export default Foods;
