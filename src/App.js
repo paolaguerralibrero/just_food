da9323fac1f2d87d761858e03d832e37f7313812
Paolaguerralibrero <mpaolamcra@hotmail.com>
2023-04-03 00:47:06 +0100

    added routing and admin user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useMediaQuery } from "react-responsive";
 import appStyles from "./App.module.css";
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Foods from "./Foods";
+import { auth, logOut } from "./auth/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
 export const foodItemsContext = React.createContext();
-
-
-
 const App = () => {
-  const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
+  const navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  const [isChooseFoodPage, setIsChooseFoodPage] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
   const isLapOrDeskTop = useMediaQuery({
     query: "(min-width: 1224px)",
   });
@@ -70,63 +75,75 @@ const App = () => {
   return (
     <foodItemsContext.Provider value={menuItems}>
       {isMobile && (
-            <div className={appStyles.App}>
+        <div className={appStyles.App}>
+          <button className={appStyles.signOutButton} onClick={logOut}>
+            Sign Out
+          </button>
+          {isAdmin && (
             <button
               className={appStyles.togglebButton}
               onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
             >
               {isChooseFoodPage ? "Availability Check" : "Order Food"}
             </button>
-            <h3 className={appStyles.titleMobile}>Just Food Online Shop</h3>
-            {!isChooseFoodPage && (
-              <Fragment>
-                <h4 className={appStyles.subTitleMobile}>Menu Availability</h4>
-                <ul className={appStyles.ulAppMobile}>
-                  {menuItems.map((item) => {
-                    return (
-                      <li key={item.id} className={appStyles.liAppMobile}>
-                        {item.name} - {item.quantity}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </Fragment>
-            )}
-            {isChooseFoodPage && <Foods foodItems={menuItems}/>}
-          </div>
+          )}
+          <span className={appStyles.signedInMessage}>
+            Signed in as {userEmail}
+          </span>
+
+          <h3 className={appStyles.titleMobile}>Just Food Online Shop</h3>
+          {!isChooseFoodPage && (
+            <Fragment>
+              <h4 className={appStyles.subTitleMobile}>Menu Availability</h4>
+              <ul className={appStyles.ulAppMobile}>
+                {menuItems.map((item) => {
+                  return (
+                    <li key={item.id} className={appStyles.liAppMobile}>
+                      {item.name} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </ul>
+            </Fragment>
+          )}
+          {isChooseFoodPage && <Foods foodItems={menuItems} />}
+        </div>
       )}
       {isLapOrDeskTop && (
-    <div className={appStyles.App}>
-      <button
-        className={appStyles.togglebButton}
-        onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
-      >
-        {isChooseFoodPage ? "Availability Check" : "Order Food"}
-      </button>
-      <h3 className={appStyles.title}>Just Food Online Shop</h3>
-      {!isChooseFoodPage && (
-        <Fragment>
-          <h4 className={appStyles.subTitle}>Menu Availability</h4>
-          <ul className={appStyles.ulApp}>
-            {menuItems.map((item) => {
-              return (
-                <li key={item.id} className={appStyles.liApp}>
-                  {item.name} - {item.quantity}
-                </li>
-              );
-            })}
-          </ul>
-        </Fragment>
+        <div className={appStyles.App}>
+          <button className={appStyles.signOutButton} onClick={logOut}>
+            Sign Out
+          </button>
+          {isAdmin && (
+            <button
+              className={appStyles.toggleButton}
+              onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
+            >
+              {isChooseFoodPage ? "Availability Check" : "Order Food"}
+            </button>
+          )}
+          <span className={appStyles.signedInMessage}>
+            Signed in as {userEmail}
+          </span>
+
+          <h3 className={appStyles.title}>Just Food Online Shop</h3>
+          {!isChooseFoodPage && (
+            <Fragment>
+              <h4 className={appStyles.subTitle}>Menu Availability</h4>
+              <ul className={appStyles.ulApp}>
+                {menuItems.map((item) => {
+                  return (
+                    <li key={item.id} className={appStyles.liApp}>
+                      {item.name} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </ul>
+            </Fragment>
+          )}
+          {isChooseFoodPage && <Foods foodItems={menuItems} />}
+        </div>
       )}
-
-
-      {isChooseFoodPage && 
-      <Foods 
-      foodItems={menuItems}
-      />}
-
-    </div>
-    )}
     </foodItemsContext.Provider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Auth from "./auth/Auth";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route exact path="/" element={<Auth />}></Route>
+      <Route exact path="/app" element= {<App />} />
+    </Routes>
   </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
